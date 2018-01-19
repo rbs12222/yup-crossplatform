32869d6b2338d400981ae506ee40a6f482195bcd
Rej Salazar <5835@ait.nsw.edu.au>
2018-01-19 11:35:02 +0000

    sidebar

--- app/Sidebar.js
+++ app/Sidebar.js
@@ -92,10 +92,15 @@ export default class Sidebar extends Component {
 }import React, { Component } from 'react';
 import { View, Text, TextInput, Dimensions } from 'react-native';
 
+import { observer } from 'mobx-react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Button from 'react-native-button';
 
+import * as firebase from 'firebase';
 
+import UserStore from './data/UserStore';
+
+@observer
 export default class Sidebar extends Component {
 
   constructor(props) {
@@ -112,18 +117,36 @@ export default class Sidebar extends Component {
   componentDidMount() {
   }
 
+  onLogout = () => {
+    try {
+      firebase.auth().signOut()
+        .then(() => {
+          UserStore.User = null;
+        })
+        .catch((err) => {
+
+        });
+
+      if (this.props.onLogout) {
+        this.props.onLogout();
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
+    const email = UserStore.User ? UserStore.User.email : '';
     return (
       <View style={{ flex: 1, backgroundColor: '#219176' }} >
         <View style={{ justifyContent: 'flex-start', alignItems: 'flex-start', margin: 10, marginTop: 70 }} >
           <Text style={{ color: 'white', fontSize: 17 }} >
-            abc@abc.com
+            {email}
           </Text>
           <Button
             containerStyle={{ marginVertical: 10 }}
-            onPress={() => {
-              //
-            }}
+            onPress={this.onLogout}
           >
             <Text style={{ color: 'white', fontSize: 17 }} >
               Logout
